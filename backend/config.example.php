ca97f8dc317d8206fcbc14658f531926cdec9b77
Hache Interactive <manglefurniture@gmail.com>
2026-08-23 11:30:23 -0500

    Add backend configuration example

---
+++ backend/config.example.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'db' => [
+        'host' => '127.0.0.1',
+        'port' => 3306,
+        'name' => 'solution_spa',
+        'user' => 'solution_spa',
+        'pass' => 'CHANGE_ME',
+        'charset' => 'utf8mb4',
+    ],
+];
